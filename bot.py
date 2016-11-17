182975eba31cb60db041cf4d7b5ad4614b4f4197
Aleksey Eremeev <eremeev.dev@gmail.com>
2016-11-18 01:35:33 +0300

    add common commands

--- bot.py
+++ bot.py
@@ -4,10 +4,22 @@ import botogram
 bot = botogram.create("277452410:AAFGQ4SDIYcHRxfB9iI55crZVwJgIL2lN8A")
 
 
-@bot.command("hello")
-def hello_command(chat, message, args):
-    """Say hello to the world!"""
-    chat.send("Hello world")
+@bot.command("add")
+def add_command(chat, message, args):
+    """Add url for check"""
+    chat.send("Add url for check")
+
+
+@bot.command("remove")
+def remove_command(chat, message, args):
+    """Remove url from check"""
+    chat.send("Remove url for check")
+
+
+@bot.command("list")
+def list_command(chat, message, args):
+    """List urls for check"""
+    chat.send("List url for check")
 
 
 if __name__ == "__main__":
