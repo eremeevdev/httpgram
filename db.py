3ae3578445d48f86ee4284d3a850ed612c095bff
Aleksey Eremeev <eremeev.dev@gmail.com>
2016-11-20 02:29:40 +0300

    add task for check status code

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
-from pymongo import MongoClient
+from datetime import datetime
+from pymongo import MongoClient, DESCENDING
 
 
 client = MongoClient('db', 27017)
@@ -22,3 +23,25 @@ def get_url_list(chat):
         links.append(result['url'])
 
     return links
+
+
+def get_all_urls():
+    return list(db.links.distinct('url'))
+
+
+def get_last_status_log(url):
+    return db.status_log.find_one({'url': url}, sort=[('date',  DESCENDING)])
+
+
+def get_chats_for_url(url):
+
+    chat_list = []
+
+    for link in db.links.find({'url': url}):
+        chat_list.append(link['chat'])
+
+    return chat_list
+
+
+def log_status(url, status_code):
+    db.status_log.insert_one({'url': url, 'status_code': status_code, 'date': datetime.now()})
--- tasks.py
+++ tasks.py
@@ -1,7 +1,36 @@
 from celery import Celery
 
+import requests
+import db
+
+from bot import bot
+
 app = Celery('tasks', broker='redis://redis:6379/0')
 
+
 @app.task
-def add(x, y):
-    return x + y
+def check_url(url):
+
+    status_code = requests.get(url).status_code
+    history_log = db.get_last_status_log(url)
+
+    if history_log is None or history_log['status_code'] != status_code:
+
+        db.log_status(url, status_code)
+
+        chat_list = db.get_chats_for_url(url)
+
+        if status_code == 200:
+            msg = 'UP: {}'.format(url)
+
+        else:
+            msg = 'DOWN: {}'.format(url)
+
+        for chat in chat_list:
+            bot.chat(chat).send(msg)
+
+
+@app.task
+def check():
+    for url in db.get_all_urls():
+        check_url.delay(url)
